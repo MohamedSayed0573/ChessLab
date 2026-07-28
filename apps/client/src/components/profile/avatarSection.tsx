77c79cca0b2e02a17d1bbf9f1a4654a55f1c02d4
Mohamed <mohamedsaid0573@gmail.com>
2026-07-28 12:13:43 +0300

    Refactor Profile.tsx

---
+++ apps/client/src/components/profile/avatarSection.tsx
@@ -0,0 +1,52 @@
+import { useApi } from "../../hooks/useApi";
+import useUser from "../../hooks/useUser";
+import defaultAvatar from "../assets/default-avatar.svg";
+
+export function AvatarSection() {
+	const { user, setUser } = useUser();
+	const fetchApi = useApi();
+	if (!user) return;
+
+	const imageUrl = user.avatarUrl;
+
+	async function handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
+		const file = e.target.files?.[0];
+		if (!file) return;
+
+		const formData = new FormData();
+		formData.append("avatar", file);
+
+		const res = await fetchApi("/users/me/avatar", {
+			method: "PATCH",
+			body: formData,
+		});
+
+		if (res.ok) {
+			const data = await res.json();
+			setUser((prev) =>
+				prev ? { ...prev, avatarUrl: data.avatarUrl } : prev,
+			);
+		}
+	}
+
+	return (
+		<div className="relative h-37.5 w-37.5 bg-white">
+			<label htmlFor="profile-image" className="cursor-pointer">
+				<span className="material-symbols-outlined absolute right-2 bottom-2">
+					edit
+				</span>
+				<input
+					id="profile-image"
+					type="file"
+					className="hidden"
+					onChange={handleImageChange}
+				/>
+				<img
+					src={imageUrl || defaultAvatar}
+					alt="Profile"
+					className="h-37.5 w-37.5 object-cover"
+				/>
+			</label>
+		</div>
+	);
+}
---
+++ apps/client/src/components/profile/deleteAccountBtn.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import { useApi } from "../../hooks/useApi";
+import { useNavigate } from "react-router";
+
+export function DeleteAccountBtn() {
+	const [open, setOpen] = useState(false);
+	const { logout } = useAuth();
+	const fetchApi = useApi();
+	const navigate = useNavigate();
+
+	async function handleDeleteAccount() {
+		try {
+			const res = await fetchApi("/users/me", {
+				method: "DELETE",
+			});
+
+			if (!res.ok) {
+				console.error("Error: Failed to delete account");
+				return;
+			}
+			navigate("/");
+
+			logout();
+		} catch (err) {
+			console.error("Error: Failed to delete account", err);
+		}
+	}
+
+	return (
+		<>
+			<button
+				type="button"
+				className="cursor-pointer bg-[#E34A4A] px-6 py-2 text-base font-medium text-[#FFFFFF]"
+				onClick={() => setOpen(!open)}
+			>
+				<p>Remove Account</p>
+
+				{open && (
+					<div className="fixed inset-0 flex cursor-default items-center justify-center bg-black/50">
+						<div className="rounded-lg bg-[#2C2927] p-6">
+							<h3 className="mb-4 text-xl font-bold text-[#E8E1DC]">
+								Are you sure you want to remove your account?
+							</h3>
+							<div className="flex justify-evenly gap-4">
+								<button
+									className="flex cursor-pointer items-center justify-center bg-[#9FD668] px-6 py-2 text-base font-medium text-[#1C3700]"
+									onClick={handleDeleteAccount}
+								>
+									<span className="material-symbols-outlined">
+										check
+									</span>
+								</button>
+
+								<button
+									type="button"
+									className="cursor-pointer border border-[#373431] px-6 py-2 text-base font-medium text-[#E8E1DC] hover:bg-[#373431]"
+									onClick={() => setOpen(!open)}
+								>
+									Close
+								</button>
+							</div>
+						</div>
+					</div>
+				)}
+			</button>
+		</>
+	);
+}
